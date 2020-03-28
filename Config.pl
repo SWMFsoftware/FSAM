e98e9b01ee5bd4f32d76e1ef9395d2532240d7b5
Xiantong Wang <xtwang@umich.edu>
2020-03-28 18:46:43 -0400

    change the herot address in the Config.pl, for transfering to GitLab

--- Config.pl
+++ Config.pl
@@ -15,7 +15,7 @@ our $Code      = "FSAM";
 my $config = "share/Scripts/Config.pl";
 
 # get util and share
-my $GITCLONE = "git clone"; my $GITDIR = "herot.engin.umich.edu:/GIT/FRAMEWORK/";
+my $GITCLONE = "git clone"; my $GITDIR = "herot:/GIT/FRAMEWORK/";
 if (-f $config or -f "../../$config"){
 }else{
     `$GITCLONE $GITDIR/share.git; $GITCLONE $GITDIR/util.git`;
